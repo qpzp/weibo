e31402c087cf37f1f4dd9fb9de14947618c83066
qpzp <qpzp@hotmail.com>
2019-11-10 11:44:35 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,7 +5,7 @@
     <p class="lead">欢迎来到我的超市</p>
     <p>这将是我人生新的开始、颤抖吧</p>
     <p>
-      <a href="#" class="btn btn-lg btn-success" role="button">现在加入</a>
+      <a href="{{route('signup')}}" class="btn btn-lg btn-success" role="button">现在加入</a>
     </p>
   </div>
 @stop
