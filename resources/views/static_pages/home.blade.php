8796146d79a82fb83878e15cbf0fb08937a12b91
qpzp <qpzp@hotmail.com>
2019-11-16 12:49:24 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.default')
 @section('content')
-  <div class="jumbotron">
-    <h1>黄志斌</h1>
-    <p class="lead">欢迎来到我的超市</p>
-    <p>这将是我人生新的开始、颤抖吧</p>
-    <p>
-      <a href="{{route('signup')}}" class="btn btn-lg btn-success" role="button">现在加入</a>
-    </p>
-  </div>
+  @if(Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info',['user'=>Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>黄志斌</h1>
+      <p class="lead">欢迎来到我的超市</p>
+      <p>这将是我人生新的开始、颤抖吧</p>
+      <p>
+        <a href="{{route('signup')}}" class="btn btn-lg btn-success" role="button">现在加入</a>
+      </p>
+    </div>
+  @endif
 @stop
