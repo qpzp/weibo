b2a5f9e926d6cbbda2b56656cee7505ec2fa0944
qpzp <qpzp@hotmail.com>
2019-11-16 09:25:40 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable
     {
         parent::boot();
         static::creating(function ($user) {
-            $user->activation_token=Str::random(10);
+            $user->activation_token = Str::random(10);
         });
     }
 
@@ -52,5 +52,10 @@ class User extends Authenticatable
     {
         $hash = md5(trim($this->attributes['email']));
         return "http://www.gravatar.com/avatar//$hash?s=$size";
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
     }
 }
