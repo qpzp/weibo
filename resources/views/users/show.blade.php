9f53a225668b5c35a30e30393bb11c272874b747
qpzp <qpzp@hotmail.com>
2019-11-17 04:28:33 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,9 +4,12 @@
   <div class="row">
     <div class="offset-md-2 col-md-8">
       <div class="col-md-12">
-        <div class="offset-md-2 col-md-8">
+        <div class="col-md-12">
           <section class="user_info">
             @include('shared._user_info',['user'=>$user])
+          </section>
+          <section class="stats mt-2">
+            @include('shared._stats',['user'=>$user])
           </section>
           <section class="status">
             @if($statuses->count()>0)
